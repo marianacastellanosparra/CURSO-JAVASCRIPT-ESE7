fff644ab7b723e88b1ab7869f27d8d0d27dc7892
marianacastellanosparra <marianacastellanosparra676@gmail.com>
2026-04-07 00:12:40 -0500

    agregue comentarios

--- 03-ModulosEs-Scope-Closures/src/app/strings.js
+++ 03-ModulosEs-Scope-Closures/src/app/strings.js
@@ -1,22 +1,21 @@
 // funcion toNonEmptyString , funcion que permite validar si un string no esta vaci
 
-function toNonEmptyString(str) {
-  if (typeof str !== 'string' || str.trim() === '') {
-    throw new Error('El valor debe ser un string no vacío');
+function toNonEmptyString(str) { // Validar que el valor es un string no vacío
+  if (typeof str !== 'string' || str.trim() === '') { // Verificar si no es un string o si es un string vacío después de eliminar espacios en blanco
+    throw new Error('El valor debe ser un string no vacío'); // Lanzar un error si la validación falla
   }
     return str;
 }
 
-function toNoEmptyString (value, label = 'texto') {
-    const str = String(value).trim();// Convertir a string y eliminar espacios en blanco
-    if (!str) {
-        throw new Error(`El ${label} no puede estar vacío`);
+function toNoEmptyString (value, label = 'texto') { // Validar que el valor es un string no vacío
+    const str = String(value).trim(); // Convertir a string y eliminar espacios en blanco
+    if (!str) { // Verificar si el string está vacío después de eliminar espacios en blanco
+        throw new Error(`El ${label} no puede estar vacío`); // Lanzar un error con un mensaje personalizado si la validación falla
     }
     return str;
 
 }
     
-
 // si esta vacio lanza un error
 
 //funcion titleCase, funcion que perASEmite convertir un string a titulo
@@ -29,3 +28,18 @@ function toNoEmptyString (value, label = 'texto') {
 }
 //funcion slugify, funcion que permite convertir un string a slug ejemplo:
 //  "hola mundo" - "hola-mundo"
+
+export function slugify(text) { // "Hola Mundo" -> "hola-mundo"
+    return text.toLowerCase() // Convertir a minúsculas Hola Mundo -> hola mundo
+        .trim() // Eliminar espacios en blanco al inicio y al final
+        .replace(/\s+/g, '-') // Reemplazar espacios en blanco por guiones
+        .replace(/[^\w-]+/g, ''); // Eliminar caracteres no alfanuméricos excepto guiones
+}
+
+//[^\w-]+:
+// \w = letras (a-z), números (0-9) y guion bajo _.
+// ^ = negación → significa "todo lo que NO sea eso".
+// - = permite guiones.
+// + = uno o más caracteres.
+// Elimina cualquier carácter raro como ! @ # % , . etc.
+// Ejemplo: "hola-mundo!" → "hola-mundo".
